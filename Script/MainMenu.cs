672cba1fcc0b1677b246c5466bdaaf135b8391ca
TimmBunes <patrickjohngarcia420@gmail.com>
2025-02-06 11:56:30 +0800

    Implement main menu functionality with button handlers and menu visibility control

--- Script/MainMenu.cs
+++ Script/MainMenu.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject startMenu;
+    public GameObject settingsMenu;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ShowMainMenu();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnStartButtonPressed()
+    {
+        mainMenu.SetActive(false);
+        startMenu.SetActive(true);
+    }
+
+    public void OnFreeplayButtonPressed()
+    {
+        // Implement Freeplay button functionality here
+    }
+
+    public void OnSettingsButtonPressed()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void OnExitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenu.SetActive(true);
+        startMenu.SetActive(false);
+        settingsMenu.SetActive(false);
     }
 }
