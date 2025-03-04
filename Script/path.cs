18c52045112ab6eeeab593f12d5bf8fe71598d7c
TimmBunes <patrickjohngarcia420@gmail.com>
2025-03-04 15:00:54 +0800

    ai path

--- Script/path.cs
+++ Script/path.cs
@@ -1,33 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using system.Collections.Generic;
 
 public class path : MonoBehaviour
 {
     // Start is called before the first frame update
-    public color lineColor;
+    public Color lineColor;
 
     private List<Transform> nodes = new List<Transform>();
-    void OnDrawGismos(){
-        Gismos.color = lineColor;
 
-        List<Transofrm> pathTransform = GetComponentsInChildren<Transform>();
+    void OnDrawGizmos()
+    {
+        Gizmos.color = lineColor;
+
+        List<Transform> pathTransforms = new List<Transform>(GetComponentsInChildren<Transform>());
         nodes = new List<Transform>();
 
-        for(int i = 0; i < pathTransform.Count; i++){
-            if(pathTransform[i] != transform){
-                nodes.Add(pathTransform[i]);
+        for (int i = 0; i < pathTransforms.Count; i++)
+        {
+            if (pathTransforms[i] != transform)
+            {
+                nodes.Add(pathTransforms[i]);
             }
         }
 
-        for (int i = 0; i < nodes.Count; i++){
+        for (int i = 0; i < nodes.Count; i++)
+        {
             Vector3 currentNode = nodes[i].position;
             Vector3 previousNode = Vector3.zero;
 
-            if(i > 0){
+            if (i > 0)
+            {
                 previousNode = nodes[i - 1].position;
-            } else if(i == 0 && nodes.Count > 1){
+            }
+            else if (i == 0 && nodes.Count > 1)
+            {
                 previousNode = nodes[nodes.Count - 1].position;
             }
 
