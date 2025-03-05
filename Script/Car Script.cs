3c2dd1931e2900764ae6c8b0b6c3e478b193ae49
TimmBunes <patrickjohngarcia420@gmail.com>
2025-03-05 20:20:31 +0800

    Enhance CarScript functionality with improved acceleration and movement logic

--- Script/Car Script.cs
+++ Script/Car Script.cs
@@ -12,14 +12,33 @@ public class CarScript : MonoBehaviour
 
     [Header("Wheel Transform")]
     public Transform WheelFLTrans;
-    public Transform WheelFRTrans;  
+    public Transform WheelFRTrans;
     public Transform WheelRLTrans;
     public Transform WheelRRTrans;
 
     [Header("Car Engine")]
-    public float acceleration = 300f;
+    public float acceleration = 1000f; // Increased acceleration value
     public float breakForce = 30000f;
     public float presentBreakForce = 0f;
     public float presentAcceleration = 0f;
 
+    private void Update()
+    {
+        MoveCar();
+    }
+
+    private void MoveCar()
+    {
+        presentAcceleration = acceleration * Input.GetAxis("Vertical");
+
+        WheelFL.motorTorque = presentAcceleration;
+        WheelFR.motorTorque = presentAcceleration;
+        WheelRL.motorTorque = presentAcceleration;
+        WheelRR.motorTorque = presentAcceleration;
+
+        WheelFLTrans.Rotate(WheelFL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        WheelFRTrans.Rotate(WheelFR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        WheelRLTrans.Rotate(WheelRL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        WheelRRTrans.Rotate(WheelRR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+    }
 }
