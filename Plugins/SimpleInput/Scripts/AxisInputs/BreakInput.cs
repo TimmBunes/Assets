083c0f5945dd145f48de408223767f8173856198
John Patrick T Garcia <144293733+TimmBunes@users.noreply.github.com>
2025-03-13 13:39:05 +0800

    Implement BreakInput class with drag functionality and deadzone support for input handling

--- Plugins/SimpleInput/Scripts/AxisInputs/BreakInput.cs
+++ Plugins/SimpleInput/Scripts/AxisInputs/BreakInput.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class BreakInput : MonoBehaviour
+namespace SimpleInputNamespace
 {
-    // Start is called before the first frame update
-    void Start()
+    public class BreakInput : MonoBehaviour, ISimpleInputDraggable
     {
-        
-    }
+        public SimpleInput.ButtonInput brakeButton = new SimpleInput.ButtonInput("Brake");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+#pragma warning disable 0649
+        [Tooltip("Radius of the deadzone at the center of the arrows that will yield no input")]
+        [SerializeField]
+        private float deadzoneRadius;
+        private float deadzoneRadiusSqr;
+#pragma warning restore 0649
+
+        private RectTransform rectTransform;
+
+        private void Awake()
+        {
+            rectTransform = (RectTransform)transform;
+            gameObject.AddComponent<SimpleInputDragListener>().Listener = this;
+
+            deadzoneRadiusSqr = deadzoneRadius * deadzoneRadius;
+        }
+
+        private void OnEnable()
+        {
+            brakeButton.StartTracking();
+        }
+
+        private void OnDisable()
+        {
+            brakeButton.StopTracking();
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            deadzoneRadiusSqr = deadzoneRadius * deadzoneRadius;
+        }
+#endif
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            CalculateInput(eventData);
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            CalculateInput(eventData);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            brakeButton.value = false;
+        }
+
+        private void CalculateInput(PointerEventData eventData)
+        {
+            Vector2 pointerPos;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, eventData.position, eventData.pressEventCamera, out pointerPos);
+
+            // Check if the brake button should be pressed
+            brakeButton.value = (pointerPos.y < 0f && Mathf.Abs(pointerPos.x) < deadzoneRadius);
+        }
     }
 }
